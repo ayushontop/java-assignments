752aefe4afff2855b60edfbf673d1446d88ce06b
Ayush Tiwari <55987406+ayushontop@users.noreply.github.com>
2022-08-28 18:10:56 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,17 @@
+package com.ayush;
+
+import java.util.Scanner;
+
+public class evenodd {
+        public static void main(String[] args) {
+            Scanner in= new Scanner (System.in);
+            int n= in.nextInt();
+            if(n%2==0)
+                System.out.println("even");
+            else
+                System.out.println("odd");
+
+
+        }
+    }
+
