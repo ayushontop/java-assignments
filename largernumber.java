e6c1198fbd6ce25529c8b4f02cf0cc935ede5ec5
Ayush Tiwari <55987406+ayushontop@users.noreply.github.com>
2022-08-28 18:21:09 +0530

    Create largernumber.java

---
+++ largernumber.java
@@ -0,0 +1,15 @@
+package com.ayush;
+
+import java.util.Scanner;
+
+public class largernumber {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        int i= in.nextInt();
+        int x=in.nextInt();
+        if(i>x)
+        System.out.println(i);
+        else
+        System.out.println(x);
+    }
+}
