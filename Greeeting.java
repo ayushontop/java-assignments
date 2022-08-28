05d0120211e9d53b6225efafbd48d6ec47d23f90
Ayush Tiwari <55987406+ayushontop@users.noreply.github.com>
2022-08-28 18:16:37 +0530

    Create Greeeting.java

---
+++ Greeeting.java
@@ -0,0 +1,13 @@
+package com.ayush;
+
+import java.util.Scanner;
+
+public class greetinginput {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        System.out.println("Hello " + a);
+    }
+}
